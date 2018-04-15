cae07858af1e320dd1cf8ed206256a3375cd9713
alexzdn <zadnipro@oregonstate.edu>
2018-04-15 01:27:13 -0700

    Improved part 1 #4
    Now prints out MSE data for particular number of features to .csv file which is used to plot graph

--- assignment_1/implementation_assignment_1.py
+++ assignment_1/implementation_assignment_1.py
@@ -21,35 +21,15 @@ Y_test = np.delete(matrix_test, np.s_[0:13], 1)
 X_train_dummy = np.insert(X_train, 0, 1, axis=1)
 X_test_dummy = np.insert(X_test, 0, 1, axis=1)
 
-#generate d random features and insert them
-row_num_train = X_train.shape[0]
-row_num_test = X_test.shape[0]
-
-mu, sigma = 0, 0.1 	#mean and standard deviation
-from copy import deepcopy
-X_train_mod_d = deepcopy(X_train)
-X_test_mod_d = deepcopy(X_test)
-
-for d in range(0, 100):
-	feature_train = np.random.normal(mu, sigma, row_num_train)
-	feature_test = np.random.normal(mu, sigma, row_num_test)
-	X_train_mod_d = np.insert(X_train_mod_d, 0, feature_train, axis=1)
-	X_test_mod_d = np.insert(X_test_mod_d, 0, feature_test, axis=1)
-
-
-
 # compute optimum weight vector
 w_dummy = np.matmul(np.linalg.inv(np.matmul(np.transpose(X_train_dummy), X_train_dummy)), np.matmul(np.transpose(X_train_dummy), Y_train))
 w = np.matmul(np.linalg.inv(np.matmul(np.transpose(X_train), X_train)), np.matmul(np.transpose(X_train), Y_train))
-w_mod_d = np.matmul(np.linalg.inv(np.matmul(np.transpose(X_train_mod_d), X_train_mod_d)), np.matmul(np.transpose(X_train_mod_d), Y_train))
 
 # compute sum of squared error (SSE)
 sse_train_dummy = 0
 sse_test_dummy = 0
 sse_train = 0
 sse_test = 0
-sse_train_mod_d = [()]
-sse_test_mod_d = [()]
 
 for idx, house in enumerate(X_train_dummy):
 	prediction = np.dot(house, w_dummy)
@@ -71,37 +51,74 @@ for idx, house in enumerate(X_test):
 	error = (Y_test[idx] - prediction) ** 2
 	sse_test += error
 
-for d in range(0, 100):
-	for idx, house in enumerate(X_train_mod_d):
-		prediction = np.dot(house, w_mod_d)
-		error = (Y_train[idx] - prediction) ** 2
-		sse_train_mod_d += (d, error)
-
-	for idx, house in enumerate(X_test_mod_d):
-		prediction = np.dot(house, w_mod_d)
-		error = (Y_test[idx] - prediction) ** 2
-		sse_test_mod_d += (d, error)
-
 # mean squared error (MSE)
 mse_train_dummy = sse_train_dummy / X_train_dummy.shape[0]
 mse_test_dummy = sse_test_dummy / X_test_dummy.shape[0]
 mse_train = sse_train / X_train.shape[0]
 mse_test = sse_test / X_test.shape[0]
 
-for d in range(0, 100):
-	mse_train_mod_d += (d, sse_train_mod_d / X_train_mod_d.shape[0])
-	mse_test_mod_d += (d, sse_test_mod_d / X_test_mod_d.shape[0])
-
 print("Training Results w/ dummy var:\nSSE: {}\tMSE: {}".format(float(sse_train_dummy), float(mse_train_dummy)))
 print("Testing Results w/ dummy var:\nSSE: {}\tMSE: {}".format(float(sse_test_dummy), float(mse_test_dummy)))
 print("Training Results:\nSSE: {}\tMSE: {}".format(float(sse_train), float(mse_train)))
 print("Testing Results:\nSSE: {}\tMSE: {}".format(float(sse_test), float(mse_test)))
-print("Training Results w/ {} additional features:\nSSE: {}\tMSE: {}".format(d, float(sse_train_mod_d), float(mse_train_mod_d)))
-print("Testing Results w/ {} additional features:\nSSE: {}\tMSE: {}".format(d, float(sse_test_mod_d), float(mse_test_mod_d)))
 
+
+
+
+#generate N random features(d), insert them and calculate
+row_num_train = X_train.shape[0]
+row_num_test = X_test.shape[0]
+features_train = []
+features_test = []
+sse_train_mod_d = []
+sse_test_mod_d = []
+mse_train_mod_d = [()]
+mse_test_mod_d = [()]
+mu, sigma = 0, 0.1 	#mean and standard deviation
+N = 100 #number of features to test with
+
+from copy import deepcopy
+X_train_mod_d = deepcopy(X_train)
+X_test_mod_d = deepcopy(X_test)
+
+#generating list of additional features
+for d in range(0, N):
+	features_train.append(np.random.normal(mu, sigma, row_num_train))
+	features_test.append(np.random.normal(mu, sigma, row_num_test))
+
+for d in range(0, N):
+	X_train_mod_d = np.insert(X_train_mod_d, 0, features_train[d], axis=1)
+	X_test_mod_d = np.insert(X_test_mod_d, 0, features_test[d], axis=1)
+
+	w_mod_d = np.matmul(np.linalg.inv(np.matmul(np.transpose(X_train_mod_d), X_train_mod_d)), np.matmul(np.transpose(X_train_mod_d), Y_train))
+
+	tmp_err = 0
+	for idx, house in enumerate(X_train_mod_d):
+		prediction = np.dot(house, w_mod_d)
+		error = (Y_train[idx] - prediction) ** 2
+		tmp_err += error
+	sse_train_mod_d.append(tmp_err)
+
+	tmp_err = 0
+	for idx, house in enumerate(X_test_mod_d):
+		prediction = np.dot(house, w_mod_d)
+		error = (Y_test[idx] - prediction) ** 2
+		tmp_err += error
+	sse_test_mod_d.append(tmp_err)
+
+for d in range(0, N):
+	mse_train_mod_d.append((d+1, sse_train_mod_d[d] / X_train_mod_d.shape[0]))
+	mse_test_mod_d.append((d+1, sse_test_mod_d[d] / X_test_mod_d.shape[0]))
+
+#write MSE data with additional features to csv file
+header_train = ["mse train"]
+header_test = ["mse test"]
+with open("mse_add_feat.csv", 'w+', newline='') as csv_mse:
+	mse_writer = csv.writer(csv_mse)
+	mse_writer.writerow(header_train)
+	mse_writer.writerows(mse_train_mod_d)
+	mse_writer.writerow(header_test)
+	mse_writer.writerows(mse_test_mod_d)
 
 
 #Logistic regression part
-
-##	for row in test_data:
-
