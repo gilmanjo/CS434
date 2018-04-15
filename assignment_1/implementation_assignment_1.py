1ea1b914952313171cbd04dec17b606eb9511090
alexzdn <zadnipro@oregonstate.edu>
2018-04-14 22:58:36 -0700

    small changes

--- assignment_1/implementation_assignment_1.py
+++ assignment_1/implementation_assignment_1.py
@@ -1,5 +1,8 @@
 import numpy as np
+import csv
 
+
+#Linear regression part
 with open("./housing_train.txt", "r") as f:
 	matrix_train = np.array([line.split() for line in f]).astype(float)
 
@@ -21,13 +24,13 @@ X_test_dummy = np.insert(X_test, 0, 1, axis=1)
 #generate d random features and insert them
 row_num_train = X_train.shape[0]
 row_num_test = X_test.shape[0]
-d = 10
+
 mu, sigma = 0, 0.1 	#mean and standard deviation
 from copy import deepcopy
 X_train_mod_d = deepcopy(X_train)
 X_test_mod_d = deepcopy(X_test)
 
-for i in range(0, d):
+for d in range(0, 100):
 	feature_train = np.random.normal(mu, sigma, row_num_train)
 	feature_test = np.random.normal(mu, sigma, row_num_test)
 	X_train_mod_d = np.insert(X_train_mod_d, 0, feature_train, axis=1)
@@ -45,8 +48,8 @@ sse_train_dummy = 0
 sse_test_dummy = 0
 sse_train = 0
 sse_test = 0
-sse_train_mod_d = 0
-sse_test_mod_d = 0
+sse_train_mod_d = [()]
+sse_test_mod_d = [()]
 
 for idx, house in enumerate(X_train_dummy):
 	prediction = np.dot(house, w_dummy)
@@ -68,23 +71,26 @@ for idx, house in enumerate(X_test):
 	error = (Y_test[idx] - prediction) ** 2
 	sse_test += error
 
-for idx, house in enumerate(X_train_mod_d):
-	prediction = np.dot(house, w_mod_d)
-	error = (Y_train[idx] - prediction) ** 2
-	sse_train_mod_d += error
+for d in range(0, 100):
+	for idx, house in enumerate(X_train_mod_d):
+		prediction = np.dot(house, w_mod_d)
+		error = (Y_train[idx] - prediction) ** 2
+		sse_train_mod_d += (d, error)
 
-for idx, house in enumerate(X_test_mod_d):
-	prediction = np.dot(house, w_mod_d)
-	error = (Y_test[idx] - prediction) ** 2
-	sse_test_mod_d += error
+	for idx, house in enumerate(X_test_mod_d):
+		prediction = np.dot(house, w_mod_d)
+		error = (Y_test[idx] - prediction) ** 2
+		sse_test_mod_d += (d, error)
 
 # mean squared error (MSE)
 mse_train_dummy = sse_train_dummy / X_train_dummy.shape[0]
 mse_test_dummy = sse_test_dummy / X_test_dummy.shape[0]
 mse_train = sse_train / X_train.shape[0]
 mse_test = sse_test / X_test.shape[0]
-mse_train_mod_d = sse_train_mod_d / X_train_mod_d.shape[0]
-mse_test_mod_d = sse_test_mod_d / X_test_mod_d.shape[0]
+
+for d in range(0, 100):
+	mse_train_mod_d += (d, sse_train_mod_d / X_train_mod_d.shape[0])
+	mse_test_mod_d += (d, sse_test_mod_d / X_test_mod_d.shape[0])
 
 print("Training Results w/ dummy var:\nSSE: {}\tMSE: {}".format(float(sse_train_dummy), float(mse_train_dummy)))
 print("Testing Results w/ dummy var:\nSSE: {}\tMSE: {}".format(float(sse_test_dummy), float(mse_test_dummy)))
@@ -92,3 +98,10 @@ print("Training Results:\nSSE: {}\tMSE: {}".format(float(sse_train), float(mse_t
 print("Testing Results:\nSSE: {}\tMSE: {}".format(float(sse_test), float(mse_test)))
 print("Training Results w/ {} additional features:\nSSE: {}\tMSE: {}".format(d, float(sse_train_mod_d), float(mse_train_mod_d)))
 print("Testing Results w/ {} additional features:\nSSE: {}\tMSE: {}".format(d, float(sse_test_mod_d), float(mse_test_mod_d)))
+
+
+
+#Logistic regression part
+
+##	for row in test_data:
+
