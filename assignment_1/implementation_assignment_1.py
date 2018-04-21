fixed L2 regularization on gradient descent algorithm

assignment_1/implementation_assignment_1.py
@@ -154,10 +154,10 @@ def main():
 
 	w_log = np.zeros(pix_train.shape[1])
 	eps = m.exp(-3)
-	nu = 0.00001
+	nu = 0.0001
 	k = 0	# num of gradient descent iterations
-	lam = [0.001, 0.01, 0.05, 1, 10, 100, 1000]
-	num_batches = 200
+	lam = [0.001, 0.01, 0.1, 1, 10, 100, 1000]
+	num_batches = 100
 
 	# negative log-likelihood over time of training, col 0 is gradient descent iteration
 	# col 1 is training, col 2 is testing
@@ -168,9 +168,9 @@ def main():
 		grad_des = np.zeros(pix_train.shape[1])
 		for i in range(pix_train.shape[0]):
 			pred_ans = 1.0 / (1.0 + m.exp(-np.dot(np.transpose(w_log), pix_train[i])))
-			grad_des += ((pred_ans - ans_train[i]) * pix_train[i]) + (0.5*lam[2]*(np.linalg.norm(w_log)**2))
+			grad_des += ((pred_ans - ans_train[i]) * pix_train[i])
 		
-		w_log -= (nu*grad_des)
+		w_log -= (nu*(grad_des + (lam[5]*w_log)))
 		k += 1
 
 		# print descent iteration